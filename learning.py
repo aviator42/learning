bf3ff19d5344c7af444d46c79b31a651c96ce1c1
aviator42 <migu3l.r4mon@gmail.com>
2019-09-30 15:50:55 -0500

    playing with math

--- learning.py
+++ learning.py
@@ -3,3 +3,7 @@ print("howdy, y'all!")
 print("i like typing this.")
 print("printing....Yay!!!")
 # what the fudge
+print("I will now count my chickens.")
+print("Hens", 25  + 30 / 6)
+print("Roosters: ", 100 - 25 * 3 % 4)
+print(3 + 2 + 1 -5 + 4 % 2 -1 / 4 + 6)
